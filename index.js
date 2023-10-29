ac726b8888a869f8536c8dba0d2af674ebf6c221
SantiagoBedoya <a.sbedoya200110@gmail.com>
2023-10-29 01:03:57 -0500

    feat: adding listeners

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
 const express = require("express");
+const axios = require('axios').default
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*',
+    methods: '*'
+  }
+});
 require("dotenv").config();
 
 app.use(express.json());
 
-let connections = {};
+let connections = {}
 
 io.on("connection", (socket) => {
   console.log("Client connected: ", socket.id);
   const userId = socket.handshake.headers.userid;
+  const token = socket.handshake.headers.token;
+
+  console.log({userId, token})
+
   connections[userId] = socket.id;
 
-  socket.on('disconnect', (reason) => {
+  socket.on('accept-trip', async (data) => {
+    try {
+      await axios.get(process.env.BE_URL + `/trips/${data.tripId}/driver-accept`, {
+        headers: {
+          Authorization: 'Bearer ' + token
+        }
+      })
+    }catch(err) {
+      console.log(err)
+    }
+  })
+
+  socket.on('disconnect', () => {
     delete connections[userId]
     console.log('available connections', connections)
   })
 });
 
 app.post("/send-notification", (req, res) => {
-  const body = req.body;
-  io.to(connections[body.userId]).emit("notifications", body.message);
+  const {recipients, data} = req.body;
+  recipients.forEach(recipient => {
+    console.log(connections[recipient])
+    io.to(connections[recipient]).emit("notifications", data);
+  });
   return res.sendStatus(200);
 });
 
